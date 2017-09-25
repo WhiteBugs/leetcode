618995cf2b327d311d27aef53ef1a6fd2374c4fa
WhiteBugs <chengjianan1996@gmail.com>
2017-09-25 21:33:27 +0800

    2017.9.25

--- Solution.java
+++ Solution.java
@@ -2,7 +2,11 @@ package com.leetcode;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Deque;
+import java.util.HashMap;
 
 
 
@@ -10,9 +14,144 @@ import java.util.Deque;
 public class Solution {
 
 	public static void main(String arg[]){
-		System.out.println(Integer.toBinaryString(Integer.highestOneBit(-2)-1));
-		System.out.println(new Solution().findComplement(-2));
+		System.out.println("");
+		
 	}
+	
+	
+	//669. Trim a Binary Search Tree
+	public TreeNode trimBST(TreeNode root, int L, int R) {
+		if(root == null) return null;
+		if(root.val<L){
+			root = trimBST(root.right, L, R);
+		}else if(root.val>R){
+			root = trimBST(root.left, L, R);
+		}else{
+			root.left = trimBST(root.left, L, R);
+			root.right = trimBST(root.right, L, R);
+		}
+		return root;
+	}
+	/*another answer of myself which can't pass the online judge but I think it is right.
+	  oh...so sad...
+	  it actually return an binary tree which only contains elements between L and R ...
+	  but the struct of the tree is not the same as the online judge's and this is the reason , I think,
+	  why I can't pass the judge.
+	
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+         ArrayList<Integer> content = getList(root , new ArrayList<>());
+         content.sort(null);
+         root = new TreeNode(L);
+         for(int i : content){
+        	 if(i<=R&&i>=L){
+        		 root = insert(root , i);
+        	 }
+         }
+         return root;
+         
+    }
+    private TreeNode insert(TreeNode tree, int i){
+    	if(tree == null){
+    		return new TreeNode(i);
+    	}
+    	int result = i>tree.val? 1:(i==tree.val? 0 : -1);
+    	if(result == -1 ){
+    		tree.left = insert(tree.left, i);
+    	}
+    	if(result == 1 ){
+    		tree.right = insert(tree.right, i);
+    	}
+    	return tree;
+    }
+    private ArrayList<Integer> getList(TreeNode root, ArrayList<Integer> content){
+    	content.add(root.val);
+    	if(root.left!=null){
+    		getList(root.left, content);
+    	}
+    	if(root.right!=null){
+    		getList(root.right, content);
+    	}
+    	return content;
+    }
+	*/
+	
+	// 575. Distribute Candies
+    public int distributeCandies(int[] candies) {
+        HashMap<Integer, Integer> saveCandies = new HashMap<>();
+    	for(int candy : candies){
+    		if(!saveCandies.containsKey(candy)){
+    			saveCandies.put(candy, 1);
+    		}
+    	}
+    	return saveCandies.size()>candies.length/2? candies.length/2: saveCandies.size();
+    }
+    
+	
+	//557. Reverse Words in a String III
+    public String reverseWords(String s) {
+        String[] items = s.split("\\s+");
+        if(items.length==1){
+        	StringBuilder answer = new StringBuilder();
+        	for(int i=0; i<items[0].length(); i++){
+        		answer.append(items[0].charAt(items[0].length()-i-1));
+        	}
+        	return answer.toString();
+        }else{
+        	StringBuilder answer = new StringBuilder();
+        	for(String item : items){
+        		answer.append(reverseWords(item));
+        		answer.append(" ");
+        	}
+        	answer.deleteCharAt(answer.toString().length()-1);
+        	return answer.toString();
+        }
+    }
+	
+	
+	
+	
+	/* 500. Keyboard Row  Online Answer
+	 * public String[] findWords(String[] words) {
+          return Stream.of(words).filter(s -> s.toLowerCase().matches("[qwertyuiop]*|[asdfghjkl]*|[zxcvbnm]*")).toArray(String[]::new);
+          }
+	 */
+    public String[] findWords(String[] words) {
+        ArrayList<String> answer = new ArrayList<>();
+        ArrayList<Character> first = new ArrayList<>(Arrays.asList('q','w','e','r','t','y','u','i','o','p'));
+        ArrayList<Character> second = new ArrayList<>(Arrays.asList('a','s','d','f','g','h','j','k','l'));
+        ArrayList<Character> third = new ArrayList<>(Arrays.asList('z','x','c','v','b','n','m'));
+        for(int j=0; j<words.length; j++){
+        	String item = words[j].toLowerCase();
+        	System.out.println(item);
+        	int queue = 0;
+        	for(int i=0; i<item.length(); i++){
+        		char c = item.charAt(i);
+        		if(first.contains(c)){
+        			if(i==0){
+        				queue = 1;
+        			}else if(queue !=1){
+        				break;
+        			}
+        		}else if(second.contains(c)){
+        			if(i==0){
+        				queue = 2;
+        			}else if(queue !=2){
+        				break;
+        			}
+        		}else if(third.contains(c)){
+        			if(i==0){
+        				queue = 3;
+        			}else if(queue !=3){
+        				break;
+        			}
+        		}
+        		if(i==item.length()-1){
+        			answer.add(words[j]);
+        		}
+        	}
+        }
+        return answer.toArray(new String[answer.size()]);
+    }
 	
 	//617. Merge Two Binary Trees
     public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
